e0474636e791c5579bd5a69eaca48599efa868b7
saranshgupta1995 <saranshgupta1995@gmail.com>
2021-05-25 20:31:39 +0530

    setup the routing and css base

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./reset.css";
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-ReactDOM.render(
-    <div>
-        Food Ordering App
-    </div>,
-    document.getElementById('root')
-);
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route path="/">
+            <div>Food Ordering App</div>
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+ReactDOM.render(App(), document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
