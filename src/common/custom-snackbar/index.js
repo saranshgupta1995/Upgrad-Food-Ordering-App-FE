b3a1bb739e37782a191033d5e36617df397b26aa
saranshgupta1995 <saranshgupta1995@gmail.com>
2021-06-14 23:34:51 +0530

    add the custom snackbar component

---
+++ src/common/custom-snackbar/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Snackbar from "@material-ui/core/Snackbar";
+import IconButton from "@material-ui/core/IconButton";
+import CloseIcon from "@material-ui/icons/Close";
+
+export default function CustomizedSnackBar({ open, onClose, message }) {
+  return (
+    <Snackbar
+      anchorOrigin={{
+        vertical: "bottom",
+        horizontal: "left"
+      }}
+      open={open}
+      autoHideDuration={3000}
+      onClose={onClose}
+      message={message}
+      action={
+        <React.Fragment>
+          <IconButton
+            size="small"
+            aria-label="close"
+            color="inherit"
+            onClick={onClose}
+          >
+            <CloseIcon fontSize="small" />
+          </IconButton>
+        </React.Fragment>
+      }
+    />
+  );
+}
